f9f2700328d14e05b1fcca495c391c13aa16452d
Darksyntax1312 <mayankmadhukar67@gmail.com>
2026-07-28 17:28:46 +0530

    Add variable naming rules exercises in script.js

---
+++ Week-1: Variables and data types/Day-3-variable-naming-rules/script.js
@@ -0,0 +1,50 @@
+// Day 3: variable naming rules (6 to 10)
+
+// Exercise 6: Valid or Invalid
+// Question: Which of these variable names are valid in JavaScript, and which are not? Explain why.
+// let 1number = 5;
+// let _score = 5;
+// let $price = 5;
+// let user-name = 5;
+
+// Solution
+
+
+// let 1number = 5;     Invalid
+// let _score = 5;      Valid
+// let $price = 5;      Valid
+// let user-name = 5;   Invalid
+
+
+
+// Exercise 7: Case sensitivity
+// Question: Are "age" and "Age" the same variable in JavaScript? Explain your answer.
+
+// Solution
+
+// No age and Age are not same variable in js as variables are case sensitive so, js treats age and Age as two different variables
+
+
+
+
+
+// Exercise 8: Reserved keywords
+// Question: Why can't you use "let" itself as a variable name, like "let let = 5;"?
+
+// solution
+
+// We can't use let itself as a variable name as js have some reserved words which you can't use to give name of a variable
+
+// Exercise 9: Naming conventions
+// Question: What is "camelCase," and why is it commonly used for variable names in JavaScript?
+
+// Solution
+
+// camel case means giving name of a variable in camel like structure without having any space like :- ageNumber here is capital and then n is capital it is commonly used so that you don't mistakely create two variables with the same name 
+
+// Exercise 10: Meaningful names
+// Question: Between "let x = 25;" and "let userAge = 25;", which is better practice and why?
+
+// Solution 
+
+// "let userAge = 25"  is the better practice cause there x is un-understandable , means this is just a imagining number which doesn't makes any sense  ......... while userAge means it is clearly saying that this is the age of the user , and this is completely understandable and makes sense
