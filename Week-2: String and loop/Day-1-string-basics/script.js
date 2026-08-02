9e8f89ae3f8720db387bf171b0d2b755875b4319
Darksyntax1312 <mayankmadhukar67@gmail.com>
2026-08-02 17:37:40 +0530

    Create string basics exercises in script.js
    Added string manipulation exercises for beginners.

---
+++ Week-2: String and loop/Day-1-string-basics/script.js
@@ -0,0 +1,46 @@
+// Day 1: String basics (No Loops Yet)
+
+// Goal: Manipulate strings directly. Understand indexing, length, and basic methods.
+
+// Q1: Create a string variable with your name. Print the first character. Print the last character.
+
+// Answer
+// let name = "Mayank";
+// console.log(name.charAt(0));
+// let result = name.length;
+// console.log(name.charAt(name.length - 1));
+
+
+
+// Q2: Create a string "JavaScript". Print the length of the string.
+
+// Answer
+// let str = "JavaScript";
+// console.log(str.length);
+
+
+
+// Q3: Create a string "Hello World". Convert it to UPPERCASE. Convert it to lowercase.
+
+// Answer
+// let strings = "Hello World";
+// let newstr = strings.toUpperCase();
+// console.log(newstr);
+// let newstr2 = strings.toLowerCase();
+// console.log(newstr2);
+
+
+
+// Q4: Create a string "I love coding". Use .slice() to extract the word "love". Print it.
+
+// Answer
+// let astring = "I love coding";
+// console.log(astring.slice(2, 6));
+
+
+
+// Q5: Create a string "apple". Use .replace() to change it to "orange". Print the result.
+
+// Answer
+// let string = "apple";
+// console.log(string.replace("apple", "orange"));
